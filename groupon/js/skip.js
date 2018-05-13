68ce079c96f01d25e31333a120eca13a79d25cb2
nero <648014999@qq.com>
2018-05-13 19:49:18 +0800

    第一次提交

---
+++ groupon/js/skip.js
@@ -0,0 +1,3 @@
+function skip(){
+    location.replace("Search.html");
+}
---
+++ mall/js/response.js
@@ -0,0 +1,8 @@
+var deviceWidth = document.documentElement.clientWidth;
+// alert(deviceWidth);
+if (deviceWidth > 750) deviceWidth = 750; {
+
+    document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+    // window.onload(){
+        // location.reload();
+}
