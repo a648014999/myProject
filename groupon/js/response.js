81f9021441b0b8c12ea2db80b360d51b1ab54466
nero <648014999@qq.com>
2018-05-17 21:36:19 +0800

    完成团购网站的轮播图和地址下拉菜单功能以及部分页面的制作

--- groupon/js/response.js
+++ groupon/js/response.js
@@ -1,8 +1,14 @@
 var deviceWidth = document.documentElement.clientWidth;
+var maxwidth = 750;
 // alert(deviceWidth);
-if (deviceWidth > 750) deviceWidth = 750; {
+if (deviceWidth > maxwidth) deviceWidth = maxwidth; {
 
-    document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+    document.documentElement.style.fontSize = deviceWidth / (maxwidth/100) + 'px';
     // window.onload(){
         // location.reload();
 }
+$(window).resize(function(){
+    // console.log(123123);
+    location.reload();
+});
+
